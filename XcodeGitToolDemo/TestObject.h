c51f4784663ca20961f187a7495e633c34ecb562
Turtle <279714010@qq.com>
2018-02-05 12:12:43 +0800

    Add a class named TestObject.

---
+++ XcodeGitToolDemo/TestObject.h
@@ -0,0 +1,13 @@
+//
+//  TestObject.h
+//  XcodeGitToolDemo
+//
+//  Created by Turtle on 2018/2/5.
+//  Copyright © 2018年 Turtle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
+@end
---
+++ XcodeGitToolDemo/TestObject.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  XcodeGitToolDemo
+//
+//  Created by Turtle on 2018/2/5.
+//  Copyright © 2018年 Turtle. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
